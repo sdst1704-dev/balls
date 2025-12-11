dcdb9baac5a691ac1224fc04bfabf5b3b8ebfa96
sergeevds.27 <sdst1704@gmail.com>
2025-12-11 14:11:55 +0300

    balls

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,52 +4,55 @@ import java.awt.event.ActionListener;
 
 class Main extends JFrame {
 
+    int i;
+
     public Main(){
         super("title");
-        setSize(420, 420);
-        JButton button1 = new JButton("1");
-        JButton button2 = new JButton("2");
-        JButton button3 = new JButton("3");
-        JButton button4 = new JButton("4");
-        JButton button5 = new JButton("5");
-        JButton button6 = new JButton("6");
-        JButton button7 = new JButton("7");
-        JButton button8 = new JButton("8");
-        JButton button9 = new JButton("9");
+        setSize(600, 600);
+        JButton [] btns = new JButton[10];
+        for (int i=0; i<10; i++) {
+            btns[i] = new JButton(String.valueOf(i));
+        }
         JButton buttonplus = new JButton("+");
         JButton buttonminus = new JButton("-");
         JButton buttonmn = new JButton("*");
         JButton buttondel = new JButton(":");
+        JButton buttonrav = new JButton("=");
         setLayout(null);
         JTextField textField = new JTextField(20);
         textField.setText("");
         textField.setBounds(0,0, 400, 20);
-        button1.setBounds(40, 40, 50, 40);
-        button2.setBounds(40, 90, 50, 40);
-        button3.setBounds(40, 140, 50, 40);
-        button4.setBounds(90, 40, 50, 40);
-        button5.setBounds(90, 90, 50, 40);
-        button6.setBounds(90, 140, 50, 40);
-        button7.setBounds(140, 40, 50, 40);
-        button8.setBounds(140, 90, 50, 40);
-        button9.setBounds(140, 140, 50, 40);
-        add(button1);
-        add(button2);
-        add(button3);
-        add(button4);
-        add(button5);
-        add(button6);
-        add(button7);
-        add(button8);
-        add(button9);
+        for (int i=0; i<10; i++) {
+            btns[i].setBounds(40, 50*i+50, 50, 40);
+        }
+        buttonplus.setBounds(190, 40, 50, 40);
+        buttonminus.setBounds(190, 90, 50, 40);
+        buttondel.setBounds(190, 140, 50, 40);
+        buttonmn.setBounds(190, 190, 50, 40);
+        buttonrav.setBounds(190, 240, 50, 40);
+        for (int i=0; i<10; i++) {
+            add(btns[i]);
+        }
+        add(buttonplus);
+        add(buttonminus);
+        add(buttondel);
+        add(buttonmn);
+        add(buttonrav);
+        double x =0;
+        double y =0;
         add(textField);
+        for (int i = 1; i<10; i++) {
+            int finalI = i;
+            btns[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    int a = finalI;
+                    textField.setText(textField.getText()+String.valueOf(a));
+                }
+            });
+        }
+        String t = textField.getText();
 
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textField.setText("1");
-            }
-        });
     }
 
     public static void main(String[] args) {
